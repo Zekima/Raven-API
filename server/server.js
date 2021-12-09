96c7e7987931d7fd42432619e92d2df9cf6760d9
Zekima <christianledaal@gmail.com>
2021-12-09 10:58:30 +0100

    dynamic port

--- server/server.js
+++ server/server.js
@@ -24,10 +24,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-const port = 3000;
-
 app.use('/api/chirps', apiRouter);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT || port, () => { console.log(`Server is running on port: ${port}`)});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
